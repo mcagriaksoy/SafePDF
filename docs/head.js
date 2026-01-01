a1b7c17ca4ff8785be42e00fb3e13dfcbf2ed5a9
Mehmet C. Aksoy <organicmehmet@gmail.com>
2026-01-01 15:32:50 +0100

    Revert google changes

--- docs/head.js
+++ docs/head.js
@@ -140,15 +140,6 @@ function createCommonHead(pageConfig) {
         "Edit PDF metadata"
       ]
     }
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FRC3NLZG1V"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-FRC3NLZG1V');
     </script>
     `;
 
